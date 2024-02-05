385af5dd1712ac1fcb56a4356c706d8b147f1804
Danny Hsu <danny539425@gmail.com>
2024-02-06 00:00:03 +0800

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,18 +4,18 @@ import Footer from './Components/Footer';
 import Homepage from './Pages/Homepage';
 import About from './Pages/About';
 import More from './Pages/More';
-import { Routes, Route } from "react-router-dom";
+import {Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
+    <Router basename="/Project6">
       <Nav />
-      <Routes basename="/Project6">
+      <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
         <Route path="/more" element={<More />} />
       </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
